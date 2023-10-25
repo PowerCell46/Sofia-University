98fb6e2aef4dd7488b639984d15847109cea2b53
Peter Gerdzhikov <116914391+PowerCell46@users.noreply.github.com>
2023-10-25 09:27:51 +0300

    Create Ships Tasks.sql

---
+++ Databases/Exercise 1 Tasks/Ships Tasks.sql
@@ -0,0 +1,38 @@
+USE ships
+
+  
+SELECT 
+	class, 
+	country 
+FROM classes
+WHERE numguns < 10;
+
+
+SELECT 
+	name as shipName
+FROM ships
+WHERE launched < 1918;
+
+
+SELECT 
+	ship, 
+	battle
+FROM outcomes WHERE result = 'sunk';
+
+
+SELECT 
+	name
+FROM ships 
+WHERE name = class;
+
+
+SELECT 
+	name 
+FROM ships 
+WHERE name LIKE 'R%';
+
+
+SELECT 
+	name 
+FROM ships 
+WHERE LEN(REPLACE(name, ' ', '')) + 1 = LEN(name);
