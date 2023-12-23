81091a804a8757cbb9599c5aecab299a8df9a914
Peter Gerdzhikov <116914391+PowerCell46@users.noreply.github.com>
2023-12-23 13:29:05 +0200

    Create Movies Tasks.sql

---
+++ Databases/Exercise 5 Tasks/Movies Tasks.sql
@@ -0,0 +1,40 @@
+USE movies;
+
+SELECT 
+	name,
+	(SELECT 
+	COUNT(Distinct studioname)
+FROM moviestar m2
+JOIN STARSIN ON
+	m2.NAME = STARSIN.STARNAME
+JOIN MOVIE ON 
+	starsin.MOVIETITLE = movie.TITLE
+WHERE m2.NAME = moviestar.name) AS "Number of Studios"
+FROM moviestar
+JOIN STARSIN ON
+	moviestar.NAME = STARSIN.STARNAME
+JOIN MOVIE ON 
+	starsin.MOVIETITLE = movie.TITLE
+GROUP BY NAME;
+
+SELECT 
+	name,
+	(SELECT 
+	COUNT(Distinct studioname)
+	FROM moviestar m2
+	JOIN STARSIN ON
+		m2.NAME = STARSIN.STARNAME
+	JOIN MOVIE ON 
+		starsin.MOVIETITLE = movie.TITLE
+	WHERE m2.NAME = moviestar.name) as "Number of Studios"
+FROM MOVIESTAR
+left join STARSIN ON
+	MOVIESTAR.name = STARSIN.STARNAME
+left join movie on 
+	STARSIN.MOVIETITLE = movie.TITLE;
+
+SELECT STARNAME
+FROM STARSIN
+WHERE MOVIEYEAR > 1990
+GROUP BY STARNAME
+HAVING COUNT(*) >= 3;
