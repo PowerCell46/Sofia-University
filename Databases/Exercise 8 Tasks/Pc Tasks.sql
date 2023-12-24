dbd53825ea8f1b3586c183c0ff427e1ddaaa2ff5
Peter Gerdzhikov <116914391+PowerCell46@users.noreply.github.com>
2023-12-25 01:37:55 +0200

    Update Pc Tasks.sql

--- Databases/Exercise 8 Tasks/Pc Tasks.sql
+++ Databases/Exercise 8 Tasks/Pc Tasks.sql
@@ -1,17 +1,60 @@
 USE PC;
 
-INSERT INTO PRODUCT
-VALUES ('C', 1100, 'PC');
+INSERT INTO PRODUCT(maker, model, type)
+VALUES 
+	('C', '1100', 'PC');
 
 INSERT INTO PC(code, model, speed, ram, hd, cd, price)
 VALUES 
-	(12, 1100, 2400, 2048, 500, 52, 299);
+	(12, '1100', 2400, 2048, 500, '52x', 299);
 
+DELETE FROM PC
+WHERE MODEL = '1100';
 
-USE PC;
+DELETE FROM PRODUCT 
+WHERE MODEL = '1100';
 
-DELETE FROM PC 
-	WHERE model = 1100;
+INSERT INTO LAPTOP(code, model, speed, ram, hd, price, screen)
+SELECT 
+	code + 100,
+	model,
+	speed, 
+	ram, 
+	hd,
+	price + 500,
+	15
+FROM PC;
 
-DELETE FROM PRODUCT
-	WHERE model = 1100;
+DELETE FROM LAPTOP 
+WHERE model IN (
+	SELECT laptop.model FROM laptop
+	JOIN PRODUCT ON 
+		LAPTOP.model = product.model
+	WHERE maker NOT IN (
+	SELECT maker FROM product
+	JOIN PRINTER ON 
+		product.model = printer.model
+	GROUP BY maker
+	)
+);
+
+SELECT * FROM PRODUCT;
+
+UPDATE PRODUCT
+SET maker = 'A'
+WHERE maker = 'B';
+
+UPDATE PC
+SET 
+	price = price / 2,
+	hd = hd + 20;
+
+UPDATE laptop
+SET screen = screen + 1
+WHERE model IN 
+(
+	SELECT laptop.model FROM laptop
+	JOIN product ON
+		laptop.model = product.model
+	WHERE maker = 'B'
+);
