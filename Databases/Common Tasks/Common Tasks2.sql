c249f89ee9427fc0552d6294c56799cedd5377f8
Peter Gerdzhikov <116914391+PowerCell46@users.noreply.github.com>
2023-12-26 16:01:54 +0200

    Update Common Tasks2.sql

--- Databases/Common Tasks/Common Tasks2.sql
+++ Databases/Common Tasks/Common Tasks2.sql
@@ -41,3 +41,87 @@ WHERE (
         GROUP BY country
     ) AS subquery_alias
 ) >= 3;
+
+/*
+	Имената на класовете, за които няма кораб, пуснат на вода след 1921 г., но имат пуснат поне един
+	кораб.
+*/
+
+SELECT CLASS FROM CLASSES
+
+WHERE (
+	SELECT 
+		COUNT(NAME)
+	FROM CLASSES C1
+	JOIN SHIPS ON
+		C1.CLASS = SHIPS.CLASS
+	WHERE 
+		C1.CLASS = CLASSES.CLASS AND
+		LAUNCHED > 1921
+) = 0 AND (
+	SELECT 
+		COUNT(NAME)
+	FROM CLASSES C2
+	JOIN SHIPS ON 
+		C2.CLASS = SHIPS.CLASS
+	WHERE 
+		C2.CLASS = CLASSES.CLASS
+) >= 1 
+GROUP BY CLASS;
+
+/*
+	За всеки кораб броя на битките, в които е бил увреден (result = ‘damaged’). Ако корабът не е участвал
+	в битки или пък никога не е бил увреждан, в резултата да се вписва 0.
+*/
+
+SELECT 
+	NAME,
+	(
+		SELECT SUM(damaged_count) AS total_damaged_count
+	FROM (
+		SELECT 
+			CASE 
+				WHEN RESULT = 'damaged' THEN 1
+				ELSE 0
+			END AS damaged_count
+		FROM BATTLES 
+		JOIN OUTCOMES ON 
+			BATTLES.NAME = OUTCOMES.BATTLE
+		RIGHT JOIN SHIPS S1 ON
+			OUTCOMES.SHIP = S1.NAME
+		WHERE S1.name = SHIPS.NAME
+	) AS subquery_alias) AS "Number of being Damaged"
+FROM SHIPS
+GROUP BY NAME
+ORDER BY 
+	"Number of being Damaged" DESC,
+	SHIPS.NAME ASC;
+
+/*
+	За всяка държава да се изведе броят на корабите и броят на потъналите кораби.
+*/
+
+SELECT 
+	COUNTRY,
+	(
+		SELECT 
+			COUNT(SHIPS.NAME)	
+		FROM SHIPS 
+		JOIN CLASSES C1 ON
+			SHIPS.CLASS = C1.CLASS
+		WHERE C1.COUNTRY = CLASSES.COUNTRY
+	) AS "Number of Ships",
+	(
+		SELECT 
+			COUNT(SHIPS.NAME)
+		FROM SHIPS 
+		JOIN CLASSES C2 ON
+			C2.CLASS = SHIPS.CLASS
+		JOIN OUTCOMES ON
+			SHIPS.NAME = OUTCOMES.SHIP
+		WHERE 
+			C2.COUNTRY = CLASSES.COUNTRY AND 
+			RESULT = 'sunk'
+	) AS "Number of Sunken Ships"
+FROM CLASSES 
+GROUP BY COUNTRY;
