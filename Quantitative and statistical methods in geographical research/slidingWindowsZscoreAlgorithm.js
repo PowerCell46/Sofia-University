6ba0bc48fb3d49c062868d9cc8294a310c3e8a9f
Peter Gerdzhikov <116914391+PowerCell46@users.noreply.github.com>
2024-03-25 21:52:57 +0200

    Create slidingWindowsZscoreAlgorithm.js

---
+++ Quantitative and statistical methods in geographical research/slidingWindowsZscoreAlgorithm.js
@@ -0,0 +1,74 @@
+function average(...args) {
+    return args.reduce((acc, curr) => acc + curr, 0) / args.length;
+}
+
+
+function zScore(dataArray) {
+    const averageValue = average(...(dataArray.map(nestedArr => nestedArr[0])));
+    const calcDeviationArr = dataArray.map(value => ((value[0] - averageValue) ** 2));
+    const standardDeviation = Math.sqrt(average(...calcDeviationArr));
+    
+    const zScoreArray = dataArray.map(value => [value[0], value[1], [Math.abs(value[0] - averageValue) / standardDeviation]]);
+    return zScoreArray;
+}
+
+
+// console.log(zScore([[10, false], [12, false], [14, false], [16, false], [18, false]]));
+
+
+function slidingWindowsZscore(matrix) {
+    let flattenedMatrix = [];
+
+    for (let row of matrix) {
+        row.forEach(value => flattenedMatrix.push([value, false]));
+    }
+
+    for (let index = 0; index < flattenedMatrix.length; index++) {
+
+        let orderedMatrix = zScore(flattenedMatrix.slice());
+        orderedMatrix.sort((arr1, arr2) => arr2[2] - arr1[2]);
+        const currentBiggestElement = orderedMatrix.filter(arr => arr[1] === false)[0];
+
+        const current2Dindex = flattenedMatrix.indexOf(currentBiggestElement);
+        const currentColumnIndex = current2Dindex % matrix[0].length;
+        const currentRowIndex = ((current2Dindex - currentColumnIndex) / matrix[0].length);
+
+        const previousColumn = currentColumnIndex - 1 > -1;
+        const nextColumn = currentColumnIndex + 1 < matrix[0].length;
+        const previousRow = currentRowIndex - 1 > -1;
+        const nextRow = currentRowIndex + 1 < matrix.length;
+
+        const elements = [
+            previousColumn && previousRow ? matrix[currentRowIndex - 1][currentColumnIndex - 1] : null,
+            previousRow ? matrix[currentRowIndex - 1][currentColumnIndex] : null,
+            previousRow && nextColumn ? matrix[currentRowIndex - 1][currentColumnIndex + 1] : null,
+            previousColumn ? matrix[currentRowIndex][currentColumnIndex - 1] : null,
+            matrix[currentRowIndex][currentColumnIndex],
+            nextColumn ? matrix[currentRowIndex][currentColumnIndex + 1] : null,
+            nextRow && previousColumn ? matrix[currentRowIndex + 1][currentColumnIndex - 1] : null,
+            nextRow ? matrix[currentRowIndex + 1][currentColumnIndex] : null,
+            nextRow && nextColumn ? matrix[currentRowIndex + 1][currentColumnIndex + 1] : null
+        ].filter(x => x !== null);
+
+        const newValue = average(...elements);
+        matrix[currentRowIndex][currentColumnIndex] = newValue;
+        flattenedMatrix[current2Dindex] = [newValue, true];
+    }
+
+    return matrix;
+}
+
+
+const matrixAfterSlidingWindowsFunc = slidingWindowsZscore(
+    [
+        [1, 19, 3, 4, 5],
+        [6, 7, 20, 9, 10],
+        [11, 12, 13, 14, 15],
+        [16, 17, 18, 19, 20]
+    ]
+);
+
+
+for (let row of matrixAfterSlidingWindowsFunc) {
+    console.log(row.join(' | '));
+} 
