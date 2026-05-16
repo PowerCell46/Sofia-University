63307e1ca908ccd5acc4d59ea983a80ed339c035
Peter Gerdzhikov <peter.gerdzhikov.contact@gmail.com>
2026-05-16 23:45:38 +0300

    Make the braces optional when submitting

--- ConstructionCoords/backend/plugins/construction_location/entities/construction_location.py
+++ ConstructionCoords/backend/plugins/construction_location/entities/construction_location.py
@@ -9,10 +9,10 @@ def build_entity(Base: DeclarativeMeta) -> type:
 
         id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
         created_at = Column(DateTime(timezone=False), server_default=func.now(), nullable=False)
-        name = Column(String, nullable=False, default="N/A")
-        name_confidence = Column(Double, nullable=False, default=0.0)
         latitude = Column(Double, nullable=False)
         longitude = Column(Double, nullable=False)
+        name = Column(String, nullable=False, default="N/A")
+        name_confidence = Column(Double, nullable=False, default=0.0)
         # user_identifier (MAC address/optional name field)
 
     return ConstructionLocation
