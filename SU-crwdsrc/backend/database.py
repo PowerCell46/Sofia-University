8f8eb32091aec032ab0af451fb5512b6f97e7338
Peter Gerdzhikov <peter.gerdzhikov.contact@gmail.com>
2026-06-02 15:39:48 +0300

    Renaming left out old project names

--- SU-crwdsrc/backend/database.py
+++ SU-crwdsrc/backend/database.py
@@ -4,7 +4,7 @@ from sqlalchemy import create_engine, Engine
 from sqlalchemy.orm import declarative_base, sessionmaker, Session, DeclarativeMeta
 
 
-engine: Engine = create_engine(f"postgresql+psycopg2://{os.environ['POSTGRES_USER']}:{os.environ['POSTGRES_PASSWORD']}@{os.environ['POSTGRES_ENDPOINT']}/construction_coords")
+engine: Engine = create_engine(f"postgresql+psycopg2://{os.environ['POSTGRES_USER']}:{os.environ['POSTGRES_PASSWORD']}@{os.environ['POSTGRES_ENDPOINT']}/su_crwdsrc_db")
 SessionLocal: sessionmaker[Session] = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 # Base class for all SQLAlchemy declarative models; holds shared metadata and registry.
 Base: DeclarativeMeta = declarative_base()
